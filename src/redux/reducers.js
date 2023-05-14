bb0a15ffac8f6e67bfbffecbd9c73d8b79b69574
Maxim <31155487+KozinM@users.noreply.github.com>
2023-05-14 22:46:36 +0200

    cleaning comments

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,14 +1,4 @@
 import { combineReducers } from "@reduxjs/toolkit";
-
-/* const initialState = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    }; */
   
     const contactsInitialState = [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -44,26 +34,3 @@ import { combineReducers } from "@reduxjs/toolkit";
       filter: filterReducer,
     }
     );
-
-  /* export const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "contacts/addContact": 
-        return {
-          ...state,
-          contacts: [...state.contacts, action.payload],
-        };
-        case "contacts/deleteContact":
-        return {
-          ...state,
-          contacts: state.contacts.filter(contact => contact.id !== action.payload),
-        };
-        case "filters/setContactsFilter":
-        return {
-          ...state,
-          filter: action.payload,
-          };
-        default:
-        return state;
-    }
-  }; */
-  
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,3 @@
-/* import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducers";
-
-
-
-
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer,enhancer); */
-
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers";
 
